a881fd6451b8dccea66dd367c6e060294cea536c
James Ouyang <52713371+jamesouyang2003@users.noreply.github.com>
2022-07-31 21:30:33 -0700

    update toCSV method

--- src/Anime.java
+++ src/Anime.java
@@ -13,43 +13,55 @@ public class Anime {
 	public String url;
 	public String title;
 	public int rank;
-	public int score;
-	public int members;
+	public String score;
+	public String members;
 	public int year;
 	public String episodes;
 	public List<String> genreTheme;
 	public List<String> licensors;
 
+	// gets text next to a key in the left info side-bar
 	private static String getText(Element info, String key) throws Exception {
 		Element e = info.selectFirst("div.spaceit_pad:has(span:contains(" + key + ":))");
 		if (e == null) throw new Exception("Cannot find info element for key \"" + key + "\"");
 		return e.ownText().trim();
 	}
 
+	// gets list of text in urls for a key in left info side-bar
 	private static List<String> getUrls(Element info, String key) {
 		return info.select("div.spaceit_pad > span:contains(" + key + ":) ~ a").stream()
-				.map((element) -> element.text().trim()).collect(Collectors.toList());
+			.map((element) -> element.text().trim())
+			.collect(Collectors.toList());
 	}
 
 	public Anime(Document doc) throws Exception {
 
 		Element info = doc.selectFirst("#content > table > tbody > tr > td > div.leftside");
-
+		
 		rank = Integer.parseInt(getText(info, "Ranked").substring(1));
-
+		
 		title = doc.select("#contentWrapper > div > div.edit-info > div.h1-title > div > h1.title-name > strong").text();
-
-		// Url
+		
 		Matcher matcher = Pattern.compile("https://myanimelist\\.net/anime/\\d+").matcher(doc.location());
 		url = matcher.find() ? matcher.group() : doc.location();
+		
+		// licensors = getUrls(info, "Licensors");
+		licensors = getUrls(info, "Licensors").stream()
+			.filter(licensor -> !licensor.equals("add some"))
+			.collect(Collectors.toList());
 
-		// Licensing
-		licensors = getUrls(info, "Licensors");
-		members = Integer.parseInt(getText(info, "Members"));
-		year = Integer.parseInt(getUrls(info, "Premiered").get(0).split(" ")[1]);
+		score = doc.selectFirst("div.anime-detail-header-stats > div.stats-block > div.score > div.score-label").text();
+		members = getText(info,  "Members");
+
+		// year = Integer.parseInt(getUrls(info, "Premiered").get(0).split(" ")[1]);
+		String[] yearData = getText(info, "Aired").split(" ");
+		// if (yearData.length > 2) year = Integer.parseInt(yearData[2]);
+		// else year = Integer.parseInt(yearData[1]);
+		year = Integer.parseInt(yearData[yearData.length > 2 ? 2 : 1]);
 		episodes = getText(info, "Episodes");
+
 		genreTheme = getUrls(info, "Genre");
-		genreTheme = getUrls(info, "Genres");
+		genreTheme.addAll(getUrls(info, "Genres"));
 		genreTheme.addAll(getUrls(info, "Theme"));
 		genreTheme.addAll(getUrls(info, "Themes"));
 	}
@@ -58,15 +70,22 @@ public class Anime {
 		return "#" + rank + ": " + title + " (" + url + ")";
 	}
 
-	private String addQuotes(String s) {
-		return "\"" + s + "\"";
-	}
+	private String addQuotes(String s) { return "\"" + s + "\""; }
+	private String dupeQuotes(String s) { return s.replace("\"", "\"\""); }
 
+	// returns row in CSV format
 	public String toCSV() {
-		return String.join(",", Arrays.asList(
-				rank + "",
-				addQuotes(title),
-				addQuotes(url),
-				addQuotes(String.join(",", licensors))));
+		return addQuotes(String.join("\",\"", Arrays.asList(
+			"=HYPERLINK(\"" + url + "\",\"" + dupeQuotes(title) + "\")",
+			String.join(",", genreTheme),
+			year+"",
+			score,
+			members,
+			episodes,
+			String.join(",", licensors)
+		).stream()
+			.map(this::dupeQuotes)
+			.collect(Collectors.toList())
+		));
 	}
 }
--- src/MALCrawler.java
+++ src/MALCrawler.java
@@ -2,8 +2,11 @@ import java.io.FileWriter;
 import java.io.PrintWriter;
 import java.util.*;
 
+import org.jsoup.HttpStatusException;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 public class MALCrawler {
 
@@ -20,13 +23,12 @@ public class MALCrawler {
 				try {
 					int i = Integer.parseInt(arg);
 					intCount++;
-					if (intCount == 1)
-						startRank = i;
-					else if (intCount == 2) endRank = i;
+					if (intCount == 1) startRank = i; // first int is start rank
+					else if (intCount == 2) endRank = i; // second int is end rank
 				} catch (NumberFormatException e) {
 					if (arg.equals("-overwrite") || arg.equals("-o"))
-						overwrite = true;
-					else file = arg;
+						overwrite = true; // use -o to overwrite the file
+					else file = arg; // last string is output csv filename
 				}
 			}
 			if (startRank < 1) startRank = 1;
@@ -40,34 +42,35 @@ public class MALCrawler {
 
 		PrintWriter out = args.file.isEmpty() ? null
 				: new PrintWriter(new FileWriter(args.file, !args.overwrite), true);
-		if (args.overwrite) out.println("rank,title,url,licensors");
+		if (args.overwrite) out.println(Anime.CSV_HEADER);
 
 		// String baseUrl = "https://myanimelist.net/topanime.php?type=tv&limit=";
 		String baseUrl = "https://myanimelist.net/topanime.php?limit=";
 		for (int i = args.startRank; i <= args.endRank; i += 50) {
 			Document doc = Jsoup.connect(baseUrl + (i - 1)).get();
-			doc.selectFirst("#content > div.pb12 > table.top-ranking-table > tbody").children().stream()
-					.filter((child) -> child.classNames().contains("ranking-list"))
-					.filter((child) -> {
-						int rank = Integer.parseInt(child.selectFirst("td.rank > span").ownText());
-						return rank >= args.startRank && rank <= args.endRank;
-					})
-					.map((child) -> child.selectFirst("td.title > a").attr("href"))
-					.forEach((url) -> {
-						try {
-							Anime anime = new Anime(Jsoup.connect(url).get());
+			Elements animeList = doc.selectFirst("#content > div.pb12 > table.top-ranking-table > tbody").children();
+			for (Element element : animeList) {
+				if (!element.classNames().contains("ranking-list")) continue; // ignore header
 
-							if (out == null) {
-								System.out.println(anime.toCSV());
-							} else {
-								System.out.println(anime);
-								out.println(anime.toCSV());
-							}
-						} catch (Exception e) {
-							System.err.println("Error while parsing anime: " + url);
-							e.printStackTrace();
-						}
-					});
+				int rank = Integer.parseInt(element.selectFirst("td.rank > span").ownText());
+				if (rank < args.startRank || rank > args.endRank) continue; // ignore rank out of specified range
+
+				String url = element.selectFirst("td.title > a").attr("href");
+				try {
+					Anime anime = new Anime(Jsoup.connect(url).get());
+	
+					if (out == null) {
+						System.out.println(anime.toCSV());
+					} else {
+						System.out.println(anime);
+						out.println(anime.toCSV());
+					}
+				} catch (Exception e) {
+					System.err.println("Error on anime: " + url);
+					e.printStackTrace();
+					System.exit(1);
+				}
+			}
 		}
 		if (out != null) out.close();
 	}
